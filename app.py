629ec8bbe3ef3a9a87dff583b476ca0761f113d2
tacco-popa <107832377+tacco-popa@users.noreply.github.com>
2024-01-06 01:49:17 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
+# Tanvir Bin Zahid 2131717042
+# Flask is a micro web framework written in Python.
+# SQLAlchemy is an open-source SQL toolkit and object-relational mapper for the Python programming
+
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
+# Configuring the database// Setting up paths////// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+# Creating tables(database) that we will be using
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
+# Defining different functionality of the program that is present in the program
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
@@ -26,6 +31,17 @@ def add():
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/edit/<int:todo_id>", methods=["GET", "POST"])
+def edit(todo_id):
+  todo = Todo.query.filter_by(id=todo_id).first()
+  if request.method == "GET":
+    return render_template("edit.html", todo=todo)
+  else:
+    title = request.form.get("title")
+    todo.title = title
     db.session.commit()
     return redirect(url_for("home"))
 
@@ -45,6 +61,9 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+# Running in debugging mode since we dont have a actual server xD
+
 if __name__ == "__main__":
+    app.app_context().push()
     db.create_all()
     app.run(debug=True)
